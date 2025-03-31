20d97031b78c961ead62d2407098ccc613956499
Mateo Palmeiro Muñiz <mateo.palmeiro@udc.es>
2025-03-31 11:21:57 +0200

    uso de tipo long para permitir entradas mayores

--- pi.c
+++ pi.c
@@ -4,7 +4,8 @@
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    int rank, size, n, i, count = 0;
+    int rank, size, i;
+    long long int n, count = 0;
     double x, y, z, pi, PI25DT = 3.141592653589793238462643;
     
     // inicializacion de mpi
@@ -14,17 +15,17 @@ int main(int argc, char *argv[]) {
     // obtener el numero total de procesos
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // proceso 0: lectura de la entrada y distribucion del valor n
+    // proceso 0: lectura de la entrada y distribucion de n
     if (rank == 0) {
         printf("enter the number of points: (0 quits) \n");
-        scanf("%d", &n);
+        scanf("%lld", &n);
         // enviar n a cada uno de los otros procesos
         for (int dest = 1; dest < size; dest++) {
-            MPI_Send(&n, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(&n, 1, MPI_LONG_LONG_INT, dest, 0, MPI_COMM_WORLD);
         }
     } else {
         // los demas procesos reciben n del proceso 0
-        MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&n, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     
     // si n es 0, se termina la ejecucion
@@ -35,24 +36,25 @@ int main(int argc, char *argv[]) {
 
     /* distribucion de la carga de trabajo:
        cada proceso calcula los puntos asignados segun su rank,
-       utilizando un bucle for con incremento de size. */
+       usando un bucle for con incremento de size */
     for (i = rank + 1; i <= n; i += size) {
         // generar puntos aleatorios en el intervalo [0,1]
         x = ((double) rand()) / ((double) RAND_MAX);
         y = ((double) rand()) / ((double) RAND_MAX);
-        // calcular la raiz cuadrada de la suma de los cuadrados
+        // calcular la distancia al origen
         z = sqrt((x * x) + (y * y));
-        // verificar si el punto se encuentra dentro del circulo de radio 1
+        // verificar si el punto esta dentro del circulo de radio 1
         if (z <= 1.0)
             count++;
     }
 
-    // recolectar los conteos parciales: el proceso 0 suma los resultados
+    // recolectar los resultados parciales
     if (rank == 0) {
-        int total_count = count;
-        int temp_count;
+        long long int total_count = count;
+        long long int temp_count;
+        // recibir la cuenta parcial de cada uno de los demas procesos
         for (int src = 1; src < size; src++) {
-            MPI_Recv(&temp_count, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&temp_count, 1, MPI_LONG_LONG_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             total_count += temp_count;
         }
         // calcular pi usando la proporcion de puntos dentro del circulo
@@ -60,7 +62,7 @@ int main(int argc, char *argv[]) {
         printf("pi is approx. %.16f, error is %.16f\n", pi, fabs(pi - PI25DT));
     } else {
         // cada proceso envia su cuenta parcial al proceso 0
-        MPI_Send(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&count, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD);
     }
     
     // finalizar mpi
