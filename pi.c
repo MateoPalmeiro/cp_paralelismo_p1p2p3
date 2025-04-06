92d6d2c12e816d4d71e127f2bc58ebd30b0522b0
Mateo Palmeiro Muñiz <mateo.palmeiro@udc.es>
2025-04-06 19:17:55 +0200

    mejora precision

--- pi.c
+++ pi.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
     int rank, size, i;
-    long long int n, count = 0;
+    int n, count = 0;
     double x, y, z, pi, PI25DT = 3.141592653589793238462643;
     
     // inicializacion de mpi
@@ -18,14 +19,14 @@ int main(int argc, char *argv[]) {
     // proceso 0: lectura de la entrada y distribucion de n
     if (rank == 0) {
         printf("enter the number of points: (0 quits) \n");
-        scanf("%lld", &n);
-        // enviar n a cada uno de los otros procesos
+        scanf("%d", &n);
+        // enviar n a cada uno de los demas procesos
         for (int dest = 1; dest < size; dest++) {
-            MPI_Send(&n, 1, MPI_LONG_LONG_INT, dest, 0, MPI_COMM_WORLD);
+            MPI_Send(&n, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
         }
     } else {
         // los demas procesos reciben n del proceso 0
-        MPI_Recv(&n, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     
     // si n es 0, se termina la ejecucion
@@ -33,11 +34,13 @@ int main(int argc, char *argv[]) {
         MPI_Finalize();
         return 0;
     }
-
-    /* distribucion de la carga de trabajo:
-       cada proceso calcula los puntos asignados segun su rank,
-       usando un bucle for con incremento de size */
-    for (i = rank + 1; i <= n; i += size) {
+    
+    // inicializar generador aleatorio con semilla distinta por proceso
+    srand(time(NULL) + rank);
+    
+    // distribucion de la carga de trabajo:
+    // cada proceso calcula los puntos asignados segun su rank, usando un bucle for con incremento de size
+    for (i = rank; i < n; i += size) {
         // generar puntos aleatorios en el intervalo [0,1]
         x = ((double) rand()) / ((double) RAND_MAX);
         y = ((double) rand()) / ((double) RAND_MAX);
@@ -47,14 +50,14 @@ int main(int argc, char *argv[]) {
         if (z <= 1.0)
             count++;
     }
-
+    
     // recolectar los resultados parciales
     if (rank == 0) {
-        long long int total_count = count;
-        long long int temp_count;
+        int total_count = count;
+        int temp_count;
         // recibir la cuenta parcial de cada uno de los demas procesos
         for (int src = 1; src < size; src++) {
-            MPI_Recv(&temp_count, 1, MPI_LONG_LONG_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&temp_count, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             total_count += temp_count;
         }
         // calcular pi usando la proporcion de puntos dentro del circulo
@@ -62,7 +65,7 @@ int main(int argc, char *argv[]) {
         printf("pi is approx. %.16f, error is %.16f\n", pi, fabs(pi - PI25DT));
     } else {
         // cada proceso envia su cuenta parcial al proceso 0
-        MPI_Send(&count, 1, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
     
     // finalizar mpi
